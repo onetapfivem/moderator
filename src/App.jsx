65eb7aebf5395e074f14832ea5c6520eafe53daa
Maximilian Osinski <maximilianosinski@maxeman.com>
2022-12-15 22:39:36 +0100

    Refactored modules -> classes & added better responsive support to Player.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,7 @@ import Player from "./components/Player.jsx";
 
 function App() {
     return (
-        <div className={"text-black dark:text-white p-5"}>
-            <h1 className={"text-xl font-bold"}>Players</h1>
-            <Player license={"1283475b2ecc03ae6d18b5a1a6248d04ac197a71"}/>
-        </div>
+        <Player license={"1283475b2ecc03ae6d18b5a1a6248d04ac197a71"}/>
     )
 }
 
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useAsync} from "react-async";
-import ApiInterface from "../modules/ApiInterface.js";
+import ApiInterface from "../classes/ApiInterface.js";
 
 const fetchPlayer = async ({ license }) => {
     const response = await ApiInterface.Request("GET", "/player/", `license=${license}`);
@@ -17,10 +17,15 @@ const Avatar = ({ url }) => {
 }
 
 const Player = ({ license }) => {
-    const { data } = useAsync({ promiseFn: fetchPlayer, license });
+    const { data, isPending } = useAsync({ promiseFn: fetchPlayer, license });
+    if(isPending) {
+        return (
+            <h1>Loading...</h1>
+        )
+    }
     if(data) {
         return (
-            <div className={"bg-light-900 dark:bg-dark-800 text-black dark:text-white m-3 flex flex-col w-fit p-5 rounded-lg shadow-lg shadow-black/10 dark:shadow-none duration-200 hover:shadow-black/25 hover:shadow-lg"}>
+            <div className={"bg-light-900 dark:bg-dark-800 text-black dark:text-white flex flex-col w-64 md:w-fit p-5 rounded-lg shadow-lg shadow-black/10 dark:shadow-none duration-200 hover:shadow-black/25 hover:shadow-lg"}>
                 <div className={"flex flex-row items-center"}>
                     <div className={"w-10 h-10 mr-2"}>
                         <Avatar url={data["avatar"]}></Avatar>
@@ -28,7 +33,7 @@ const Player = ({ license }) => {
                     <h1 className={"font-bold text-xl truncate max-w-xs"}>{data["username"]}</h1>
                 </div>
                 <div className={"flex flex-row items-center"}>
-                    <p className={"text-xs text-light-500"}>{data["license"]}</p>
+                    <p className={"text-xs text-light-500 w-32 md:w-fit truncate"}>{data["license"]}</p>
                     <button onClick={() =>{navigator.clipboard.writeText(data["license"]).then()} } className={"text-xs outline-none bg-light-800 dark:bg-dark-600 ml-2 p-1 duration-200 rounded-md ring-green-400/50 active:ring-4"}>Kopieren</button>
                 </div>
                 <hr className={"border-2 rounded-md border-black dark:border-white w-full my-2"}></hr>
